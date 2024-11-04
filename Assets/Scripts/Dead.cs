6a4928954f733da0764f178f3af0fd36116f06e2
namgy1 <namgiyun535@gmail.com>
2024-11-04 18:34:28 +0900

    GameOver StopAll

--- Assets/Scripts/Dead.cs
+++ Assets/Scripts/Dead.cs
@@ -23,11 +23,38 @@ public class Dead : MonoBehaviour
         uIController = FindAnyObjectByType<UIController>();
     }
 
+    private void StopAll()
+    {
+        // 3D Rigidbody ∏ÿ√ﬂ±‚
+        Rigidbody[] allRigidbodies = FindObjectsOfType<Rigidbody>();
+        foreach (Rigidbody rb in allRigidbodies)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+        // 2D Rigidbody ∏ÿ√ﬂ±‚
+        Rigidbody2D[] allRigidbodies2D = FindObjectsOfType<Rigidbody2D>();
+        foreach (Rigidbody2D rb2D in allRigidbodies2D)
+        {
+            rb2D.velocity = Vector2.zero;
+            rb2D.angularVelocity = 0f;
+        }
+
+        // 2D ConstantForce ∏ÿ√ﬂ±‚
+        ConstantForce2D[] allConstantForces2D = FindObjectsOfType<ConstantForce2D>();
+        foreach (ConstantForce2D cf2D in allConstantForces2D)
+        {
+            cf2D.force = Vector2.zero;
+            cf2D.torque = 0f;
+        }
+    }
     
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (isReleased && other.gameObject.CompareTag("Dead"))
         {
+            StopAll();
             uIController.GameOver();
             resultScore = CombineBalloon.currentScore;
             GameManager.Instance.resultScoreText.text = resultScore.ToString();
